e6adf57d7839d7b660874fbcff3d8961695a911c
İrem <iremcrnblt@gmail.com>
2022-10-18 14:02:46 +0300

    README changes

---
+++ CafeBoston.DATA/CafeData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CafeBoston.DATA
+{
+    public class CafeData
+    {
+        public int TableCount { get; set; }
+        public List<Product> Products { get; set; }
+        public List<Order> ActiveOrder{ get; set; }
+        public List<Order> PastOrder { get; set; }
+    }
+}
---
+++ CafeBoston.DATA/Order.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CafeBoston.DATA
+{
+    public class Order
+    {
+        public int TableNo { get; set; }
+        public OrderState State { get; set; }
+        public decimal PaidAmount { get; set; }
+        public DateTime? StartTime { get; set; }
+        public DateTime? EndTime { get; set; }
+        public LinkedList<OrderDetail> OrderDetails { get; set; }
+        public readonly string TotalPriceTRY;
+        //public decimal TotalPrice()
+        //{
+            
+        //}
+    }
+}
